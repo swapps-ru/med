8e1137989529b92b30a65ee4723654786249f58e
swing1991 <s.o.c.i.a.l@yandex.ru>
2021-07-13 23:23:41 +0300

    Auto git pull 2

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -8,10 +8,11 @@ class GitController extends Controller
 {
     public function gitPull(Request $request)
     {
-        if($request->input('pass') !== env('GIT_PULL_SECRET', 'youshallnotpassbastard'))
+        if($request->input('pass') !== env('GIT_PULL_SECRET', 'youshallnotpassbastard') && !empty(env('GIT_PULL_URL')))
         {
             return abort(404);
         }
-        printf('<b>Result: </b> %s', shell_exec('cd ' . base_path() . ' && git reset HEAD --hard && git clean -i -fd && git pull'));
+
+        printf('<b>Result: </b> %s', shell_exec('cd ' . base_path() . ' && git reset HEAD --hard && git clean -f && git pull ' . env('GIT_PULL_URL') . ' 2>&1'));
     }
 }
