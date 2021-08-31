0f4722648b849105ce898c90ef78d10a687536f1
n1ckjansens <n1ckjansens@yandex.ru>
2021-08-31 16:13:35 +0300

    login fix

--- resources/js/Components/Input.js
+++ resources/js/Components/Input.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import WysiwygEditor from '@/utils/wysiwyg';
 
 export default function Input({
     type = 'text',
@@ -20,22 +19,20 @@ export default function Input({
     }, []);
 
     return (
-        <WysiwygEditor>
-            <div className="flex flex-col items-start">
-                <input
-                    type={type}
-                    name={name}
-                    value={value}
-                    className={
-                        `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
-                        className
-                    }
-                    ref={input}
-                    autoComplete={autoComplete}
-                    required={required}
-                    onChange={(e) => handleChange(e)}
-                />
-            </div>
-        </WysiwygEditor>
+        <div className="flex flex-col items-start">
+            <input
+                type={type}
+                name={name}
+                value={value}
+                className={
+                    `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
+                    className
+                }
+                ref={input}
+                autoComplete={autoComplete}
+                required={required}
+                onChange={(e) => handleChange(e)}
+            />
+        </div>
     );
 }
